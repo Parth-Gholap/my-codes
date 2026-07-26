91b8602917352e45b39b3250045e46db4a6fe14b
parth <your-email@example.com>
2026-07-26 22:08:27 +0530

    binary search

---
+++ binary search/normalll.java
@@ -0,0 +1,30 @@
+// package binary search;
+
+public class normalll {
+    public static void main(String[] args) {
+        int arr [] = {1,2,3,4,5,6,7,8,9};
+        int tar = 8;
+        int ans = binarynum(arr, tar);
+        System.out.println(ans);
+    }
+
+    static int binarynum(int arr[], int tar){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start<end) {
+            int mid = start + (end - start)/2;
+            if(arr[mid] == tar){
+                return mid;
+            }
+            else if(arr[mid]>tar){
+                end = mid -1;
+            }
+            else if (arr[mid]<tar){
+                start = mid + 1 ;
+
+            }
+        }
+        return -1;
+    }
+}
